8bcf4194032ef2c4449a68e4cc0da320613656c7
Peter Kronenberg <peter.a.kronenberg@rtx.com>
2024-07-01 19:39:14 -0400

    Rename module join table

--- calibration/models/module.py
+++ calibration/models/module.py
@@ -9,7 +9,7 @@ class Module(BaseModel):
     is_active = models.BooleanField()
     name = models.TextField(unique=True, null=False)
     ngen_cal_active = models.BooleanField()
-    groups = models.ManyToManyField(ModuleGroup)
+    groups = models.ManyToManyField(ModuleGroup, db_table='module_group_members')
 
     class Meta:
         db_table = 'module'
