1835b72f77f3e3bea2ee500a42186a38d6caaf29
Peter Kronenberg <peter.a.kronenberg@rtx.com>
2024-07-09 16:35:46 -0400

    Add start-up function

--- calibration/apps.py
+++ calibration/apps.py
@@ -1,6 +1,16 @@
+import os
+
 from django.apps import AppConfig
 
 
 class CalibrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'calibration'
+
+    # Use this function to do any database clean-up, such
+    # as identifying any CalibrationRun or ValidationRuns in Running status
+    # https://stackoverflow.com/questions/33814615/how-to-avoid-appconfig-ready-method-running-twice-in-django
+    def ready(self):
+        # Put model imports here
+        if os.environ.get('RUN_MAIN'):
+            print('start up')
