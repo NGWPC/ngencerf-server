b70987d5863009a886a0f04a66a2efa212130e32
Peter Kronenberg <peter.a.kronenberg@rtx.com>
2024-07-19 12:34:29 -0400

    Remove url field in domain table

--- calibration/models/domain.py
+++ calibration/models/domain.py
@@ -7,7 +7,6 @@ class Domain(BaseModel):
     description = models.TextField(null=False, blank=False)
     is_active = models.BooleanField(null=False, default=True)
     name = models.TextField(unique=True, null=False)
-    forcing_url = models.TextField(null=True)
 
     class Meta:
         db_table = 'domain'
