4605fb15a21bfd095653ae26ba80e28656a6ba3e
Peter Kronenberg <peter.a.kronenberg@rtx.com>
2024-07-29 11:40:13 -0400

    Update README with instructions for running pre_start as well as installing ngen and ngen-cal

--- runCerf.sh
+++ runCerf.sh
@@ -3,6 +3,13 @@
 cerfServer=~/projects/cerfServer/
 
 source $cerfServer/.venv/bin/activate
+
+echo
+echo Running pre_start
+$cerfServer/manage.py pre_start
+
+echo
+echo Starting server
 $cerfServer/manage.py runserver localhost:8000
 
 deactivate
