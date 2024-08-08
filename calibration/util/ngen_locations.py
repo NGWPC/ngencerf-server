01ff03b65d1c1fbc6a9ac821f7975fea7926f46e
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2024-08-08 09:51:58 -0400

    Fix venv location

--- calibration/util/ngen_locations.py
+++ calibration/util/ngen_locations.py
@@ -23,6 +23,6 @@ noah_parameter_dir = os.path.join(settings.NGEN_CAL_WORK_DIR, 'bme_config/Noah-O
 
 calibration_py = os.path.join(calib_valid_dir, 'calibration.py')
 validation_py = os.path.join(calib_valid_dir, 'validation.py')
-create_input_dir = os.path.join(NGEN_CAL_VENV, 'venv.cal/lib/python3.11/site-packages/createInput')
+create_input_dir = os.path.join(NGEN_CAL_VENV, 'lib/python3.11/site-packages/createInput')
 
-libs = [ngen_exe, cfe_lib, sloth_lib, topmd_lib, noah_lib, sft_lib, smp_lib, lasam_lib, calibration_py, validation_py]
+files = [ngen_exe, cfe_lib, sloth_lib, topmd_lib, noah_lib, sft_lib, smp_lib, lasam_lib, calibration_py, validation_py, create_input_dir]
