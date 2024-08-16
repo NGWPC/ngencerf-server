fda9902d00c6f093358ec44a7ccc13f53a32514f
David Gillingham <david.gillingham@rtx.com>
2024-08-16 10:40:28 -0500

    NGWPC-3273: Create initial docker container for ngencerf-server.

--- cerfServer/__local_settings.py
+++ cerfServer/__local_settings.py
@@ -36,7 +36,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': os.getenv('CERF_SERVER_DATABASE_PASSWORD', 'postgres'),
-        'HOST': 'localhost',
+        'HOST': os.getenv('CERF_SERVER_DATABASE_HOST', 'localhost'),
         'PORT': 5432,
         'OPTIONS': {
             'connect_timeout': 10,
@@ -44,8 +44,3 @@ DATABASES = {
         }
     }
 }
-
-# url of the front-end
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
