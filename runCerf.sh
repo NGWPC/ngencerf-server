f71cf21718547f0d2ea88cdef2fb74db6b42acda
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2024-09-15 10:39:04 -0400

    add --load-static flag to runCerf

--- runCerf.sh
+++ runCerf.sh
@@ -4,21 +4,33 @@
 
 source "./cerfserver.env"
 
-
 cerfServer="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+# Check for the --load-static flag
+LOAD_STATIC_DATA=false
+for arg in "$@"; do
+  case $arg in
+    --load-static)
+    LOAD_STATIC_DATA=true
+    shift
+    ;;
+  esac
+done
 
 if [ -n "${CERF_VENV}" ] ; then
     # shellcheck disable=SC1090
     source "$cerfServer/${CERF_VENV}/bin/activate"
 else
-    echo "CERF_VENV is not set.  PLease set the virtual environment variable."
+    echo "CERF_VENV is not set. Please set the virtual environment variable."
+    exit 1
 fi
 
 echo
 echo "Running migrate"
 python3 manage.py migrate
 
-if [ ! -f "${CERF_LOAD_STATIC_DATA}" ] ; then
+# Only load static data if the flag is provided or the CERF_LOAD_STATIC_DATA file doesn't exist
+if [ "$LOAD_STATIC_DATA" = true ] || [ ! -f "${CERF_LOAD_STATIC_DATA}" ] ; then
     echo
     echo "Loading ngenCERF static data"
 
@@ -26,7 +38,11 @@ if [ ! -f "${CERF_LOAD_STATIC_DATA}" ] ; then
         --username admin \
         --password admin \
         --email admin@nextgenwaterprediction.com
-    python3 manage.py init_sql; \
+    echo
+    echo "Calling init_sql"
+    python3 manage.py init_sql
+    echo
+    echo "Calling init_gages"
     python3 manage.py init_gages
 
     touch "${CERF_LOAD_STATIC_DATA}"
