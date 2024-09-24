45e0f2b399cfd2d8f60235068767befc58fb2f7b
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2024-09-24 09:25:56 -0400

    Change order of groups

--- hydrofabric_test_data/hydrofabric_test_data.py
+++ hydrofabric_test_data/hydrofabric_test_data.py
@@ -408,6 +408,18 @@ hydrofabric_module_metadata_real_data = {"modules": [
 # For testing
 module_sample_data = {"modules": [
     {
+        "module_name": "Topoflow",
+        "description": "description of module",
+        "module_version": {
+            "commit_hash": "CFE:d290f1ee-6c54-4b01-90e6-d701748f0851",
+            "version_url": "https://www.acme-corp.com",
+            "version_date": "2024-08-29T09:12:33.001Z"
+        },
+        "groups": [
+            "Glacier"
+        ]
+    },
+    {
         "module_name": "Noah-OWP-Modular",
         "description": "description of module",
         "module_version": {
@@ -575,18 +587,7 @@ module_sample_data = {"modules": [
     #     "groups": [
     #         "Coastal"
     #     ]
-    # },
-    {
-        "module_name": "Topoflow",
-        "description": "description of module",
-        "module_version": {
-            "commit_hash": "CFE:d290f1ee-6c54-4b01-90e6-d701748f0851",
-            "version_url": "https://www.acme-corp.com",
-            "version_date": "2024-08-29T09:12:33.001Z"
-        },
-        "groups": [
-            "Glacier"
-        ]
-    }
+    # }
+
 ]
 }
