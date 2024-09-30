2e7c82f36d73dbd856cbd8d2ec716202589c93df
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2024-09-30 14:25:53 -0400

    Initialize modules and groups Update models

--- calibration/models/calibration_formulation.py
+++ calibration/models/calibration_formulation.py
@@ -1,27 +1,26 @@
 from django.db import models
 
 from calibration.models.base_model import BaseModel
+from calibration.models.calibration_run import CalibrationRun
+from calibration.models.module import Module
 
 
 class CalibrationFormulation(BaseModel):
-    description = models.TextField(null=False)
-    name = models.CharField(max_length=50, null=False)
-    groups = models.TextField(null=False)
-    used_by_calibration_run = models.BooleanField(default=False)
+    description = models.TextField()
+    module = models.ForeignKey(Module, null=False, on_delete=models.RESTRICT)
     bmi_config_path = models.CharField(max_length=255, null=True)
-    version = models.TextField(null=True)
-    calibration_run = models.ForeignKey('CalibrationRun', null=False, on_delete=models.CASCADE)
+    calibration_run = models.ForeignKey('CalibrationRun', null=False, on_delete=models.RESTRICT)
+    module_commit_hash = models.CharField(max_length=50, null=True)
 
     class Meta:
         db_table = 'calibration_formulation'
         constraints = [
-            models.UniqueConstraint(fields=['name', 'calibration_run'], name='calibration_formulation__name__calibration_run__unique')
+            models.UniqueConstraint(fields=['module', 'calibration_run'], name='calibration_formulation__module__calibration_run__unique')
         ]
 
     def __str__(self):
         return (
             f"CalibrationFormulation: {self.id}, "
-            f"name: {self.name:20}, "
-            f"used_by_calibration_run: {str(self.used_by_calibration_run):<5}, "
+            f"name: {module.name:20}, "
             f"calibration_run: {self.calibration_run.id}"
         )
