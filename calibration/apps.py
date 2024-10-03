8622441f5dc5a51b2e3a829f34eae201069c449c
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2024-10-03 17:29:11 -0400

    Make sure logging directory is created properly

--- calibration/apps.py
+++ calibration/apps.py
@@ -2,6 +2,8 @@ import logging
 
 from django.apps import AppConfig
 
+from cerfServer.settings import NGEN_LOGGING_DIR
+
 logger = logging.getLogger(__name__)
 
 
@@ -10,6 +12,10 @@ class CalibrationConfig(AppConfig):
     name = 'calibration'
 
     def ready(self):
+
+        # Make sure the logging directory exists
+        NGEN_LOGGING_DIR.mkdir(exist_ok=True)
+
         from calibration.util.ngen_locations import check_files
 
         check_files()
--- cerfServer/settings.py
+++ cerfServer/settings.py
@@ -197,7 +197,6 @@ NGEN_CAL_REPO_ROOT = str(Path(REPO_ROOT) / 'ngen-cal')
 NGEN_CAL_MOUNT_POINT = os.getenv('NGEN_CAL_MOUNT_POINT', str(Path.home() / 'ngwpc/data'))
 
 NGEN_LOGGING_DIR = Path(NGEN_CAL_MOUNT_POINT) / 'ngencerf-server-logs'
-NGEN_LOGGING_DIR.mkdir(exist_ok=True)
 
 NGEN_STATIC_DIR = Path(NGEN_CAL_MOUNT_POINT) / 'ngen-static-files'
 
