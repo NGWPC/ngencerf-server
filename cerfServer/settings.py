8130319b111d6f8461647d77a12575aaaa8a6252
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2024-10-18 09:08:50 -0400

    Enable Hydrofabric

--- cerfServer/settings.py
+++ cerfServer/settings.py
@@ -168,9 +168,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # -----------------------------
 # Hydrofabric
 # -----------------------------
-HYDROFABRIC_GEOPACKAGE_ENDPOINT = (False, 'hydrofabric/2.1/geopackages?gage_id={gage_id}&source={source}&domain={domain}')
-HYDROFABRIC_MODULE_METADATA_ENDPOINT = (False, 'hydrofabric/2.1/modules/parameters/')
-HYDROFABRIC_OBSERVATION_DATA_ENDPOINT = (False, 'hydrofabric/2.1/observational?gage_id={gage_id}&source={agency}&domain={domain}')
+HYDROFABRIC_GEOPACKAGE_ENDPOINT = (True, 'hydrofabric/2.1/geopackages?gage_id={gage_id}&source={source}&domain={domain}')
+HYDROFABRIC_MODULE_METADATA_ENDPOINT = (True, 'hydrofabric/2.1/modules/parameters/')
+HYDROFABRIC_OBSERVATION_DATA_ENDPOINT = (True, 'hydrofabric/2.1/observational?gage_id={gage_id}&source={agency}&domain={domain}')
 HYDROFABRIC_FORCING_DATA_ENDPOINT = (False, 'hydrofabric/2.1/forcing')
 
 HYDROFABRIC_URL = os.getenv('HYDROFABRIC_URL', 'http://localhost:8001')
