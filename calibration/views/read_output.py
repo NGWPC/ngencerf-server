348e54b59f45be466de21a3de63ab481449302a2
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2024-10-25 16:04:13 -0400

    More performance updates; add retries

--- calibration/views/read_output.py
+++ calibration/views/read_output.py
@@ -2,6 +2,7 @@ import csv
 import logging
 import os
 import re
+import time
 from collections import deque
 from datetime import timedelta
 from itertools import groupby
@@ -569,11 +570,20 @@ def parse_performance_metrics(file_path):
     the .batch line, while Reserved is only on the non-batch line.
     """
 
-    if not os.path.exists(file_path):
-        logger.error(f'Performance metrics file {file_path} not found')
-        return
+    # Slurm takes awhile to finish writing the performance metrics file, so wait awhile
+    time.sleep(30)   # Shouldn't really need to wait this long
+    # Check if file exists, retrying a few times
+    attempts = 1
+    for attempt in range(attempts):
+        if os.path.exists(file_path):
+            logger.info(f'Reading performance metrics from {file_path}')
+            break
+        else:
+            logger.warning(f'Attempt {attempt + 1}: Performance metrics file {file_path} not found, retrying...')
+            time.sleep(30)
     else:
-        logger.info(f'Reading performance metrics from {file_path}')
+        logger.error(f'Performance metrics file {file_path} not found after {attempts} attempts')
+        return
 
     reserved_time = None
     batch_metrics = None
