a5ca829c00bfd99d2a9d102f976797d2611ae305
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2024-12-02 16:57:02 -0500

    Update get_status output

--- calibration/util/calibration_validators.py
+++ calibration/util/calibration_validators.py
@@ -755,6 +755,7 @@ class PerformanceMetricsSerializer(BaseSerializer):
     max_disk_read = serializers.CharField(max_length=50, required=True, allow_null=True)
     max_disk_write = serializers.CharField(max_length=50, required=True, allow_null=True)
     reserved_time = serializers.DurationField(required=False, allow_null=True)
+    io_throughput = serializers.CharField(max_length=50, required=True, allow_null=True)
 
 
 class CommonStatusFieldsMixin(serializers.Serializer):
