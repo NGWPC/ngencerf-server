6db1631c4af8b87b9d71cec6d2f1f478e6c3d83e
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2024-12-03 09:20:17 -0500

    Add new version.env file

--- calibration/util/calibration_validators.py
+++ calibration/util/calibration_validators.py
@@ -363,6 +363,7 @@ class ValidationJobsParameter(BaseSerializer):
 class FooterResponseSerializer(BaseSerializer):
     version = serializers.CharField(required=True)
     contact_email = serializers.CharField(required=True)
+    release_date = serializers.CharField(required=True)
 
 
 def validate_automatic_validation(value):
