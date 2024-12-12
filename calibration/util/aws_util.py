999aa08dbc94d21ce8e021bf49cadd6df3237677
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2024-12-12 18:52:04 -0500

    Better error checking for mounted buckets

--- calibration/util/aws_util.py
+++ calibration/util/aws_util.py
@@ -1,5 +1,6 @@
 import logging
-from pathlib import Path
+import os
+from typing import List
 
 from calibration.views.common import CerfException
 from django.conf import settings
@@ -8,25 +9,30 @@ logger = logging.getLogger(__name__)
 logging.getLogger('boto').setLevel(logging.INFO)
 
 
-def parse_s3_uri(s3_uri) -> str:
+def parse_s3_uri(s3_uri: str) -> List[str]:
     """
-      Parse an S3 URI into bucket and key components.
+    Parse an S3 URI into bucket and key components.
 
-      :param s3_uri: A string representing the S3 URI (e.g., 's3://bucket-name/key')
-      :return: A tuple containing the bucket name and key as strings
-      """
+    :param s3_uri: A string representing the S3 URI (e.g., 's3://bucket-name/key')
+    :return: A list containing the bucket name and key as strings
+    """
     return s3_uri.replace("s3://", "").split("/", 1)
 
 
-def convert_s3_uri_to_fs(uri) -> str:
+def convert_s3_uri_to_fs(s3_uri: str) -> str:
     """
-    Until Data Services gives ua a file path, convert the S3 uri to filepath
-    :param uri:
-    :return:file spec of the locally mounted bucket
+    Until Data Services gives us a file path, convert the S3 URI to a local file path.
+
+    :param s3_uri: A string representing the S3 URI (e.g., 's3://bucket-name/key')
+    :return: File path corresponding to the locally mounted bucket
     """
-    if not uri:
-        raise CerfException('uri cannot be empty')
+    if not s3_uri:
+        raise CerfException('S3 URI cannot be empty')
 
-    bucket, key = parse_s3_uri(uri)
+    bucket, key = parse_s3_uri(s3_uri)
 
-    return str(Path(settings.S3_MOUNT_POINT) / bucket / key)
+    bucket_path = os.path.join(settings.S3_MOUNT_POINT, bucket)
+    if os.path.isdir(bucket_path):
+        return os.path.join(bucket_path, key)
+    else:
+        raise CerfException(f"Cannot find mapping for bucket '{bucket}' at '{bucket_path}'")
