26c180b02a992d67e318da2c0afec4602524c2d0
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2025-01-06 13:05:29 -0500

    Workaround for EDS not returning description for output variables

--- calibration/views/data_services.py
+++ calibration/views/data_services.py
@@ -214,7 +214,8 @@ def get_module_metadata_from_data_services(run: CalibrationRun, calibration_form
                 ModuleOutputVariable.objects.update_or_create(
                     name=o['variable'],
                     calibration_formulation=calibration_formulation,
-                    defaults={'description': o['description']}
+                    # TODO Fix this.  Description is required
+                    defaults = {'description': o['description'] if o['description'] else 'placeholder description'}
                 )
             # Save parameters
             parameters = module['calibrate_parameters']
