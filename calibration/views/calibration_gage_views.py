6f6792ecceac8241a0397045fb51655491269bac
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2025-02-14 10:52:56 -0500

    Display 'undefined' for missing station name

--- calibration/views/calibration_gage_views.py
+++ calibration/views/calibration_gage_views.py
@@ -156,6 +156,8 @@ def get_gage(request: Request) -> Response:
 
     gage_id = validator.get('gage_id')
     gage = get_gage_by_id(gage_id)
+    if not gage['station_name']:
+        gage['station_name'] = "<undefined>"
 
     if not gage:
         return ResponseError(f"Gage '{gage_id}' does not exist", http_status=status.HTTP_404_NOT_FOUND)
