2c7c92bfaf6c1f3ceeeb7409bb828305f281fe4b
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2025-03-13 12:14:16 -0400

    Check if geopackage exists

--- calibration/views/data_services.py
+++ calibration/views/data_services.py
@@ -139,7 +139,7 @@ def get_geopackage_from_data_services(run: CalibrationRun):
         s3_uri = eds_data.get('uri')
         run.geopackage_eds_file_path = convert_s3_uri_to_fs(s3_uri)
         if run.geopackage_eds_file_path and not os.path.exists(run.geopackage_eds_file_path):
-            logger.error(f"Geopackage from Data Services, {run.geopackage_eds_file_path} does not exist")
+            raise DataServicesException(f"Geopackage from Data Services, {run.geopackage_eds_file_path} does not exist")
         logger.info(f'Setting run.geopackage_eds_file_path to {run.geopackage_eds_file_path}')
 
 
