f91bae9c2ebe67f3dc644063d4993895d2927ac2
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2025-03-14 10:55:31 -0400

    Don't use cache when loading dependencies

--- runCerf.sh
+++ runCerf.sh
@@ -72,7 +72,7 @@ if [ "${CERF_VENV}" != "Docker" ]; then
        echo "Installing createInput"
        if pip show "createInput" > /dev/null 2>&1; then
            # Package is installed, reinstall without dependencies
-           pip install --force-reinstall --no-deps -e "git+https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/ngen-cal.git@${NGEN_CAL_BRANCH}#egg=createInput&subdirectory=python/createInput"
+           pip install --force-reinstall --no-deps --no-cache-dir -e "git+https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/ngen-cal.git@${NGEN_CAL_BRANCH}#egg=createInput&subdirectory=python/createInput"
        else
            # Package is not installed, install with dependencies
            pip install -e "git+https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/ngen-cal.git@${NGEN_CAL_BRANCH}#egg=createInput&subdirectory=python/createInput"
@@ -82,7 +82,7 @@ if [ "${CERF_VENV}" != "Docker" ]; then
        echo "Installing swe_mapping"
        if pip show "swe_mapping" > /dev/null 2>&1; then
            # Package is installed, reinstall without dependencies
-           pip install --force-reinstall --no-deps -e "git+https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/ngen-forcing.git@${NGEN_FORCING_BRANCH}#egg=swe_processing&subdirectory=swe_processing"
+           pip install --force-reinstall --no-deps --no-cache-dir -e "git+https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/ngen-forcing.git@${NGEN_FORCING_BRANCH}#egg=swe_processing&subdirectory=swe_processing"
        else
            # Package is not installed, install with dependencies
            pip install -e "git+https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/ngen-forcing.git@${NGEN_FORCING_BRANCH}#egg=swe_processing&subdirectory=swe_processing"
