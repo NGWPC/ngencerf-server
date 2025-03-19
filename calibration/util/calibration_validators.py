5cf23f82526dc9124636dd83490312461045e017
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2025-03-19 12:06:30 -0400

    Make modules optional

--- calibration/util/calibration_validators.py
+++ calibration/util/calibration_validators.py
@@ -337,7 +337,7 @@ class CalibrationJobsResponseSerializer(BaseSerializer):
     objective_function = serializers.CharField(required=False, allow_null=True)
     optimization_algorithm = serializers.CharField(required=False, allow_null=True)
     validations = serializers.ListSerializer(child=ValidationStatusSerializer(), required=False, allow_empty=True)
-    modules = serializers.ListSerializer(child=serializers.CharField(required=True, allow_null=False, allow_blank=False), required=True, allow_empty=True)
+    modules = serializers.ListSerializer(child=serializers.CharField(required=True, allow_null=False, allow_blank=False), required=False)
     is_archived = serializers.BooleanField(required=True, allow_null=True)
 
 
