2f947505004373022a48b2568364c5b006345729
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2025-06-17 16:10:27 -0400

    Fix merge error

--- calibration/views/calibration_landing_views.py
+++ calibration/views/calibration_landing_views.py
@@ -362,8 +362,8 @@ def clone_job(request: Request) -> Response:
         response['warnings'] = warnings
     if errors:
         response['errors'] = errors
-    if messages:
-        response.setdefault('warnings', []).extend(messages)
+    if response_dict:
+        response.setdefault('warnings', []).extend(response_dict)
 
     response_validator, error_response = validate_response(ImportResponseSerializer, response)
     if error_response:
