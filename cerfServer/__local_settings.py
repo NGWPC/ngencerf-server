df020df5f4ca16eb8a8e45f4872e159082636bb4
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2025-08-06 16:52:57 -0400

    More db diagnostics

--- cerfServer/__local_settings.py
+++ cerfServer/__local_settings.py
@@ -32,6 +32,7 @@ DATABASES = {
         'PASSWORD': os.getenv('CERF_SERVER_DATABASE_PASSWORD', 'postgres'),
         'HOST': os.getenv('CERF_SERVER_DATABASE_HOST', 'localhost'),
         'PORT': 5432,
+        'CONN_MAX_AGE': 10,
         'OPTIONS': {
             'connect_timeout': 10,
             'options': '-c statement_timeout=10000ms'
