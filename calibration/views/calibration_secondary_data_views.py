ed52bb15f55e1a104f8153ef3a13177f60c9d74a
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2025-12-10 13:34:53 -0500

    Revert "Clean up performance timing"
    This reverts commit 78fd75991dd7620e0c4913baf5f3d524da7da022.

--- calibration/views/calibration_secondary_data_views.py
+++ calibration/views/calibration_secondary_data_views.py
@@ -140,9 +140,9 @@ def get_or_create_secondary_plots(run: ValidationRun, date: str, data_type: Seco
             '--direct_s3'
         ]
         logger.info(f"Calling {cfg['map_func'].__name__} with arguments: {args}")
-        start_time = time.perf_counter()
+        start_time = time.time()
         cfg["map_func"](args)
-        elapsed_time = time.perf_counter() - start_time
+        elapsed_time = time.time() - start_time
         logger.info(f"Finished running {cfg['map_func'].__name__} in {elapsed_time:.2f} seconds")
     else:
         logger.info(f"{prefix.upper()} files already exist in {plot_dir} for {get_job_description(run)}")
@@ -201,9 +201,9 @@ def generate_secondary_ts_data(validation_run: ValidationRun, data_type: Seconda
     ]
 
     logger.info(f"Calling {ts_func.__name__} with arguments: {args}")
-    start_time = time.perf_counter()
+    start_time = time.time()
     ts_func(args)
-    elapsed_time = time.perf_counter() - start_time
+    elapsed_time = time.time() - start_time
     logger.info(f"Finished running {ts_func.__name__} in {elapsed_time:.2f} seconds")
 
 
