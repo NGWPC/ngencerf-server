6c8bc054030c1575b824198b8289d1b7e9c1a172
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2026-01-14 13:49:55 -0500

    Adjust forecast settings; remove unused fields

--- calibration/util/calibration_validators.py
+++ calibration/util/calibration_validators.py
@@ -1240,14 +1240,12 @@ class MPINodesRulesResponseSerializer(GenericMessageResponseSerializer):
 class ForecastConfigSerializer(BaseSerializer):
     name = serializers.CharField(required=True, validators=[enum_validator(ForecastConfigEnum)])
     data_sources = serializers.CharField(required=False, allow_null=True)
-    time_range = serializers.CharField(required=False, allow_null=True)
     domain = serializers.CharField(required=False, validators=[enum_validator(DomainEnum)])
     availability_lag = serializers.IntegerField(required=True)
     cycle_start = serializers.IntegerField(required=True)
     cycle_end = serializers.IntegerField(required=True)
     cycle_freq = serializers.IntegerField(required=True)
     fcst_win = serializers.IntegerField(required=True)
-    fcst_timestep = serializers.FloatField(required=True)
 
 
 class LoadForecastTabResponseSerializer(BaseSerializer):
