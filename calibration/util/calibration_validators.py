707ead2ed3ff5e06659661c1a20e3a2a974d1190
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2026-01-28 17:07:27 -0500

    Return cold_start date in get_status

--- calibration/util/calibration_validators.py
+++ calibration/util/calibration_validators.py
@@ -1095,6 +1095,7 @@ class GetStatusForValidationResponseSerializer(CommonStatusFieldsMixin, Validati
 
 class GetStatusColdStartSerializer(BaseSerializer):
     cold_start_run_id = serializers.IntegerField(required=True)
+    cold_start_date = serializers.DateTimeField(required=True)
     status = serializers.CharField(required=True)
     submit_date = serializers.DateTimeField(required=False, allow_null=True)
     sent_date = serializers.DateTimeField(required=False, allow_null=True)
@@ -1109,7 +1110,6 @@ class GetStatusForForecastResponseSerializer(CommonStatusFieldsMixin, ForecastRu
     message = serializers.CharField(required=False)
     configuration = serializers.CharField(required=True, validators=[enum_validator(ForecastConfigEnum)])
     cycle_date = serializers.DateTimeField(required=True, allow_null=False)
-    cold_start_date = serializers.DateTimeField(required=False, allow_null=True)
     cold_start_run = GetStatusColdStartSerializer(required=False, allow_null=True)
 
 
