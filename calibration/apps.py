f5211e82403d918d648d576683479ce68a3d9543
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2026-07-01 11:31:34 -0400

    Correct incorrect logging

--- calibration/apps.py
+++ calibration/apps.py
@@ -87,15 +87,13 @@ class CalibrationConfig(AppConfig):
         if running_dev_server or running_gunicorn:
             logger.info(f'NGENCERF_BASE_URL: {settings.NGENCERF_BASE_URL}\n')
             logger.info(f'ENTERPRISE_DATA_URL: {settings.ENTERPRISE_DATA_URL}\n')
-            logger.info(f'CONTAINER_DATA_ROOT: {settings.CONTAINER_DATA_ROOT}')
-            logger.info(f'HOST_DATA_ROOT: {settings.HOST_DATA_ROOT}')
+            logger.info(f'NGEN_CAL_MOUNT_POINT: {settings.NGEN_CAL_MOUNT_POINT}')
             logger.info(f'NGEN_STATIC_DIR: {settings.NGEN_STATIC_DIR}')
             logger.info(f'NGENCERF_ARCHIVE_S3_PATH: {settings.NGENCERF_ARCHIVE_S3_PATH}')
             logger.info(f'NGENCERF_ZIPS_S3_PATH: {settings.NGENCERF_ZIPS_S3_PATH}')
             logger.info(f"FORCING_AORC_CONUS_BMI_DATE_RANGE: {settings.FORCING_AORC_CONUS_BMI_DATE_RANGE}")
             logger.info(f'DJANGO DEBUG: {settings.DEBUG}')
-            logger.info(f"MPI_NODE_RULES: {settings.MPI_NODE_RULES}")
-            logger.info(f"NODE_TYPE_RULES: {settings.SLURM_NODE_TYPE_RULES}")
+            log_mpi_rules()
 
             from calibration.util.ngen_locations import check_files
 
